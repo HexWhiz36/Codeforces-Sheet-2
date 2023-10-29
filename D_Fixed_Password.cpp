c2a0de56d5e32dba65ec6d7b7704d66d622d37a7
5H40N <shaonshah36@gmail.com>
2023-10-30 01:14:32 +0600

    Add files via upload

---
+++ D_Fixed_Password.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a;
+    for(int i=1;;)
+    {
+        cin>>a;
+        if(a==1999)
+        {
+            cout<<"Correct"<<endl;
+            break;
+        }
+        else{cout<<"Wrong"<<endl;}
+    }
+    
+    return 0;
+}
